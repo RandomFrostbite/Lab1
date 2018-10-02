ac43b7db547f03f3ed56069236c115b774366091
RandomFrostbite <il.zainutdinov@gmail.com>
2018-10-02 20:15:43 +0200

    Implemented individual task #3

--- app/src/main/java/com/nvwa/lab1/MainActivity.java
+++ app/src/main/java/com/nvwa/lab1/MainActivity.java
@@ -28,6 +28,39 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submit(View view) {
-        //task 3
+        TextView code = findViewById(R.id.code);
+        TextView food = findViewById(R.id.food);
+
+        Random rnd = new Random();
+        int codeNum = -1;
+        try {
+            codeNum = Integer.parseInt(code.getText().toString());
+        } catch( Exception e ) {}
+
+        if ( codeNum == 42  && food.getText().toString().toLowerCase().equals( "not a cake" ) ) {
+            code.setHint( getString(R.string.code11) );
+            food.setHint( getString(R.string.food11) );
+        } else if ( codeNum == 42 || food.getText().toString().toLowerCase().equals( "not a cake" ) ) {
+            code.setHint( getString(R.string.code10) );
+            food.setHint( getString(R.string.food10) );
+        } else if ( code.getText().length() == 0 || food.getText().length() == 0 ) {
+            code.setHint( getString(R.string.code01) );
+            food.setHint( getString(R.string.food01) );
+        } else {
+            code.setHint( getString(R.string.code00) );
+            food.setHint( getString(R.string.food00) );
+        }
+
+        code.setBackgroundColor( Color.rgb( rnd.nextInt() % 256, rnd.nextInt() % 256, rnd.nextInt() % 256));
+        code.setTextColor( Color.rgb( rnd.nextInt() % 256, rnd.nextInt() % 256, rnd.nextInt() % 256));
+        code.setHintTextColor( Color.rgb( rnd.nextInt() % 256, rnd.nextInt() % 256, rnd.nextInt() % 256));
+
+        food.setBackgroundColor( Color.rgb( rnd.nextInt() % 256, rnd.nextInt() % 256, rnd.nextInt() % 256));
+        food.setTextColor( Color.rgb( rnd.nextInt() % 256, rnd.nextInt() % 256, rnd.nextInt() % 256));
+        food.setHintTextColor( Color.rgb( rnd.nextInt() % 256, rnd.nextInt() % 256, rnd.nextInt() % 256));
+        
+        code.setText("");
+        food.setText("");
+
     }
 }
