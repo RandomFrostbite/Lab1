a32de9e2dd582edaf94103e25e6f6ff9351fe245
RandomFrostbite <il.zainutdinov@gmail.com>
2018-10-02 18:06:39 +0200

    Implemented tasks, described in the pdf

--- app/src/main/java/com/nvwa/lab1/MainActivity.java
+++ app/src/main/java/com/nvwa/lab1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.nvwa.lab1;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void changeColor(View view) {
+        TextView textView = findViewById(R.id.newButton);
+        Random rnd = new Random();
+        textView.setTextColor(Color.rgb(
+                rnd.nextInt() % 256,
+                rnd.nextInt() % 256,
+                rnd.nextInt() % 256));
+
     }
 }
